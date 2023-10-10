02b7f60aba4789b149fd418e7413aa874ce6c3e1
jinops <jinwoo.develop@gmail.com>
2023-10-10 17:40:26 +0900

    Yolo with rotated imgs 2

--- cam.py
+++ cam.py
@@ -2,37 +2,49 @@ import cv2
 import torch
 import imutils
 
-def get_imgs():
-    ret, img = cam.read()
+def get_imgs(img, angle=10):
+    
     imgs = []
-    for i in range(0, 91, angle):
+    angles = []
+    for i in range(0, 180+1, angle):
         imgs.append(imutils.rotate(img, i))
-    return imgs
-
+        angles.append(i)
+    return imgs, angles
 
 def get_diff(xmin, ymin, xmax, ymax):
     return (xmax - xmin) - (ymax - ymin)
 
-# Load the model
+def get_rotated_angle(results, angles):
+    max_diff = None
+    angle = None
+
+    for i in range(len(angles)):
+        xys = results.pandas().xyxy[i]
+        if len(xys) == 0:
+            continue
+        xy = xys.loc[0] # TODO: This code is only for 1 object
+        diff = get_diff(xy.xmin, xy.ymin, xy.xmax, xy.ymax)
+        if max_diff == None or diff > max_diff :
+            max_diff = diff
+            angle = angles[i]
+            print(xy.confidence)
+    return angle
+    
+
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+# cam = cv2.VideoCapture(0)
+# ret, img = cam.read()
+img = cv2.imread('./img_sample.jpeg')
 
-# Set webcam input
-cam = cv2.VideoCapture(0)
+imgs, angles = get_imgs(img)
 
-time = 1
-angle = 10
-
-while time > 0:
-    imgs_rotate = get_imgs()
-    # if (cv2.waitKey(1) & 0xFF == ord("q")) or (cv2.waitKey(1)==27):
-    #     break
-    time -= 1
-
-results = model(imgs_rotate)
-#results.show()
+results = model(imgs)
 results.save()
-print(results.pandas().xyxy[0].xmin)  # img1 predictions (tensor)
+
+angle = get_rotated_angle(results, angles)
+
+print(angle)  # img1 predictions (tensor)
 
 # Close the camera
-cam.release()
+# cam.release()
 cv2.destroyAllWindows()
