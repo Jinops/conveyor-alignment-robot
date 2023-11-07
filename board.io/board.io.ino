89b2d61f0e48249704caa4a3a9dd0d10309c6756
jinops <jinwoo.develop@gmail.com>
2023-11-07 17:37:15 +0900

    Add command num for testing

--- board.io/board.io.ino
+++ board.io/board.io.ino
@@ -2,30 +2,19 @@
 #include <ParameterList.h> // Define rx/tx port here!
 
 MyCobotBasic myCobot;
-int incomingByte;
+String SerialData;
+int num;
 
-void init_angles(int speed=20){
-  myCobot.setLEDRGB(0XFF, 0, 0);
-  myCobot.writeAngle(1, 0, speed);
-  delay(1000);
-  myCobot.writeAngle(2, 0, speed);
-  delay(1000);
-  myCobot.writeAngle(3, 0, speed);
-  delay(1000);
-  myCobot.writeAngle(4, 0, speed);
-  delay(1000);
-  myCobot.writeAngle(5, 0, speed);
-  delay(1000);
-  myCobot.writeAngle(6, 0, speed);
-  delay(1000);
-  myCobot.setLEDRGB(0, 0, 0XFF);
+void initAngles(){
+  myCobot.setLEDRGB(byte(255), 0, 0);
+  Angles angles = {0, 0, 0, 0, 0, 0};
+  myCobot.writeAngles(angles, 20);
+  delay(200);
+  myCobot.setLEDRGB(0, 0, byte(255));
 }
 
 void test(){
-  myCobot.writeAngle(2, -60, 50);
-  delay(1000);
-  myCobot.writeAngle(3, -30, 50);
-  delay(1000);
+  myCobot.writeAngles({0.08,98.43,-117.33,-56.07,0.26,0.52}, 20);
 }
 
 void blink(int count, int delay_ms=100){
@@ -37,28 +26,66 @@ void blink(int count, int delay_ms=100){
   }
 }
 
-void setup() {
-  myCobot.setup();
-  myCobot.powerOn();
+void printAngles(){
+  Serial.print(" (Angles) {");
+  for (int i=0; i<=5; i++){
+    Serial.print(myCobot.getAngles()[i]);
+    (i < 5) ? Serial.print(",") : NULL;
+  }
+  Serial.print('}');
+}
 
+void printCoords(){
+  Serial.print(" (Coords) {");
+  for (int i=0; i<=5; i++){
+    Serial.print(myCobot.getCoords()[i]);
+    (i < 5) ? Serial.print(",") : NULL;
+  }
+  Serial.print('}');
+}
+
+void setup() {;
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
-  init_angles(50);
-  Serial.begin(9600);   // Cobot uses Serial1 
+  myCobot.setup();
+  myCobot.powerOn();
+  initAngles();
 
-  // test();
+  Serial.begin(9600); // USB Serial / Cobot uses Serial1
 }
 
 void loop() {
   if (Serial.available()) {
-    incomingByte = Serial.parseInt();
+    SerialData = Serial.readString();
+    Serial.print("(Serial) " + SerialData);
+    num = SerialData.substring(0, 1).toInt();
 
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
-
-    if (incomingByte <= 255){
-      myCobot.setLEDRGB(255, byte(incomingByte), 255);
+    if(num==0){
+      printAngles();
+      printCoords();
+    }
+    else if (1 <= num && num <= 6){
+      int angle = SerialData.substring(1).toInt();
+      myCobot.writeAngle(num, angle, 20);
+    } 
+    else if(num==7){
+      initAngles();
+    }
+    else if (num==8){
+      test();
+    }
+    else if(num==9){ // moter ON / OFF
+      Serial.print(myCobot.isPoweredOn());
+      if(myCobot.isPoweredOn()){
+        myCobot.setLEDRGB(0, byte(50), 0);
+        myCobot.powerOff();
+      } else {
+        myCobot.powerOn();
+        myCobot.setLEDRGB(0, 0, byte(255));
+        printAngles();
+        printCoords();
+      }
     }
   }
 }
