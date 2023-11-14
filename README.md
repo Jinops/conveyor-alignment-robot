d6822e900d0dce0d588bcbf4f403e477d53c6340
jinops <jinwoo.develop@gmail.com>
2023-11-14 16:09:26 +0900

    Add board_main

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # conveyor-alignment-robot
 WIP (OCT.2023 ~ DEC.2023)
+![flow diagram](./flow_diagram.png)
+
---
+++ board/board_main/board_main.ino
@@ -0,0 +1,88 @@
+#include <MyCobotBasic.h>
+#include <ParameterList.h> // serial port is defined here depend on board. (for me, MyCobot_Mega)
+
+MyCobotBasic myCobot;
+int serialInput;
+
+Coords coords_0 = {32.50,-60.40,93.60,93.68,11.88,-81.59};
+Coords coords_100 = {133.00,-48.40,93.50,91.59,-20.93,-82.74};
+Coords coordsToMove;
+
+int speed = 30; // movement speed
+int msToStay = 2000; // time for moving+staying
+int IdleAddY = 10; // adding Y of Coord after stay
+
+void initAngles(){
+  Angles angles = {0, 0, 0, 0, 0, 0};
+  myCobot.writeAngles(angles, 20);
+}
+
+void setColorIfMoving(){ // TODO: Check Performance
+  if(myCobot.checkRunning()){
+    myCobot.setLEDRGB(byte(255), 0, 0);
+  }
+  else {
+    myCobot.setLEDRGB(0, 0, byte(255));
+  }
+}
+
+void printCoords(){
+  Serial.print(" (Coords) {");
+  for (int i=0; i<=5; i++){
+    Serial.print(myCobot.getCoords()[i]);
+    (i < 5) ? Serial.print(",") : NULL;
+  }
+  Serial.print('}');
+}
+
+void setCoords(Coords* coords){
+  if(myCobot.isPoweredOn()){
+    myCobot.setLEDRGB(0, 30, 0);
+    myCobot.powerOff();
+  } else {
+    myCobot.powerOn();
+    *coords = myCobot.getCoords();
+    printCoords();
+  }
+}
+
+void move(int length){
+  for (int i = 0; i < Axes; i++){
+    coordsToMove[i] = coords_0[i] + (coords_100[i] - coords_0[i]) * length / 100;
+  }
+  myCobot.writeCoords(coordsToMove, speed);
+  delay(msToStay);
+  // myCobot.writeCoord(Y, myCobot.getCoords()[Y]+IdleAddY, speed);
+  printCoords();
+}
+
+void setup() {
+  myCobot.setup();
+  myCobot.powerOn();
+  initAngles();
+
+  Serial.begin(9600); // USB Serial. FYI, Cobot uses Serial1 on Mega board.
+}
+
+void loop() {
+  setColorIfMoving();
+  if (Serial.available()) {
+    serialInput = Serial.parseInt();
+    Serial.print("(Serial) " + String(serialInput) + " / ");
+
+    if(serialInput==-1){
+      setCoords(&coords_0);
+    }
+    else if(serialInput==-2){
+      setCoords(&coords_100);
+      coords_100['Y'] = coords_0['Y'];
+    }
+    else if (0 <= serialInput && serialInput <=100){
+      move(serialInput);
+    }
+    else{
+      Serial.print("out of range");
+    }
+    Serial.println();
+  }
+}
