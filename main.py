f649de15b5ce761d8966b11893b990a2b3d0b76b
jinops <jinwoo.develop@gmail.com>
2023-11-16 20:13:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import serial
-import time
 import cv2
+import time
+import math
 
 interval_sec = 1
 
@@ -11,27 +12,51 @@ arduino = serial.Serial(
   )
 
 def get_image():
-  # cam = cv2.VideoCapture(0)
-  # ret, img = cam.read()
+  cam = cv2.VideoCapture(1)
+  ret, img = cam.read()
   # img = cv2.imread('./img_sample.jpeg')
-  # return img
-  return None
+  # cv2.imwrite('./test.jpg', img)
 
-def get_distance(img):
+  return img
+
+def get_distance():
   # get model's result from img
-  num = input("Enter a number: ") # Test
+  num = int(input("Enter a number: ")) # Test
+  # num = 480
   return num
 
+def get_distance_100(distance, img_height):
+  print(str(math.floor((distance/img_height*100))))
+  return str(math.floor((distance/img_height*100)))
+             
 def serial_write(x):
   arduino.write(bytes(x, 'utf-8'))
   time.sleep(1) # Because async
   data = arduino.readline()
   return data
 
+def display(img, distance):
+  height = img.shape[0]
+  width = img.shape[1]
+  img = cv2.line(img, (width//2, height), (width//2, height-distance), (0,0,255), 10)
+  cv2.imshow('window', img)
+  cv2.waitKey(1) & 0xFF == ord('0')
+
+####
+
+mode = input('Select mode - (Enter) Automative (0) Manual : ')
+
+while mode==2:
+  data = input("Input Serial Data (q to close): ")
+  if data=='q':
+    break
+  serial_write(data)
+
 while True:
   img = get_image()
-  distance = get_distance(img)
-  result = serial_write(distance)
+  distance = get_distance()
+  display(img, distance)
+  distance_100 = get_distance_100(distance, img.shape[0])
+  result = serial_write(distance_100)
   print(result)
   time.sleep(interval_sec)
-
