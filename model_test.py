dc223bdb57cfc107ad0fde2c795892187b8aaa1d
Jinops <jinwoo.develop@gmail.com>
2023-12-04 02:16:54 +0900

    Update temp

--- model_test.py
+++ model_test.py
@@ -2,7 +2,7 @@ import torch
 import sys
 # sys.path.insert(0, './model/yolov5_obb')
 model = torch.hub.load('./model/yolov5_obb', 'custom', source='local', path='./model/best.pt')
-im = './model/test.jpg'
+im = './model/test2.jpg'
 results = model(im)
-results.show()
-# results.pandas().xyxy[0]
+# results.show()
+print(results.pandas().xyxy[0])
