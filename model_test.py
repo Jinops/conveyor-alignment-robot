e92188fab19c6d5f62ef8bc9fe0478f0b69d3f15
Jinops <jinwoo.develop@gmail.com>
2023-12-03 22:44:19 +0900

    modify model test

--- model_test.py
+++ model_test.py
@@ -1,4 +1,8 @@
 import torch
 import sys
-sys.path.insert(0, './model/yolov5_obb')
-model = torch.load('model/best.pt')
+# sys.path.insert(0, './model/yolov5_obb')
+model = torch.hub.load('./model/yolov5_obb', 'custom', source='local', path='./model/best.pt')
+im = './model/test.jpg'
+results = model(im)
+results.show()
+# results.pandas().xyxy[0]
