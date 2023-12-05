bb7ad78dae10182daff187241c54fbd2dc27f039
Jinops <jinwoo.develop@gmail.com>
2023-12-05 16:59:02 +0900

    Update server.py

--- model/server.py
+++ model/server.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
 import uvicorn
 import shutil
+import os
+import yolov5_obb.detect as model
 
 app = FastAPI()
 ## test
@@ -10,8 +12,28 @@ async def main():
   return "OK"
 
 @app.post("/predict")
-async def add_staff(file: UploadFile):
-  open(file.filename, 'wb')
-      
-  return {"message": f"Successfully uploaded {file.filename}"}
+async def add_staff(file: UploadFile=File(..., media_type='image/jpeg')):
+  with open(file.filename, 'wb') as f:
+    file.file.seek(0)
+    shutil.copyfileobj(file.file, f)
+
+  # current_dir=os.path.dirname(os.path.realpath(__file__))
+  current_dir = './'
+
+  weights=os.path.join(current_dir, 'best.pt')
+  source=os.path.join(current_dir, file.filename)
+
+  result = model.run(
+  weights=weights,
+  source=source,
+  conf_thres=0.01,
+  imgsz=(416,416),
+  device=0,
+  agnostic_nms=True,
+  return_result=True,
+  classes=0
+  )
+  #return {"boxes": result}
+  return {"boxes": None}
+
 uvicorn.run(app, host="0.0.0.0", port=8000)
